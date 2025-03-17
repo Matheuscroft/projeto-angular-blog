77d99f68502956f22994fd4ee23b9a221e0d03a0
Matheus OS <matheus.sdoliveira@gmail.com>
2025-03-16 21:58:16 -0300

    alteração para DC BLOG

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
     {
       "id":"1",
-      "title": "NOVO HOMEM DE FERRO EM 3D",
-      "description": "marvel anuncia um novo filme do homem de ferro, confira",
-      "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+      "title": "NOVO AQUAMAN EM BREVE?",
+      "description": "O que a DC fala sobre um terceiro filme do heroi.",
+      "photoCover":"https://www.otempo.com.br/content/dam/otempo/editorias/entretenimento/filmes-e-series/2023/9/entretenimento-aquaman-2-veja-sinopse-elenco-e-trailer-do-filme-1708758077.jpeg"
     },
     {
       "id":"2",
-      "title": "Nova Série anunciada no Disney +",
-      "description": "bla blabla",
-      "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+      "title": "Série da Arlequina é renovada",
+      "description": "Série é um grande sucesso entre os fãs.",
+      "photoCover":"https://cinepop.com.br/wp-content/uploads/2023/01/Harley-Quinn-Valentines-Day-Special-2023.jpg"
+    },
+    {
+      "id":"3",
+      "title": "Henry Cavill pode voltar a ser Superman",
+      "description": "Fortes rumores indicam a volta do ator.",
+      "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrKC4VseUqi5UbGLPU8SdbJEnXVcTw686rpQ&s"
+    },
+    {
+      "id":"4",
+      "title": "Filme da Liga da Justiça em produção",
+      "description": "Trata-se da continuação do primeiro filme.",
+      "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6jDJyCJaD6XOnPPGi56J0sp8K-Sc5WAI9Cw&s"
     }
   ]
